2b45565c779dd20848c0d6d8bbc5f0e55e177840
Mark Turner <mark@amerine.net>
2010-10-15 23:19:49 -0700

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+gem 'mysql2'
+
+group :development, :test do
+  gem 'rspec-rails', '>= 2.0.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pintme::Application.config.secret_token = 'f9aab82094f846f01b7f44d9f1514a786ec78c6273a2d919899ae4b6482cc54f6019a79fe57cda792bce7f913e27d6ef71ec2ca9cf33ba7d6546fad2d82eada2'
