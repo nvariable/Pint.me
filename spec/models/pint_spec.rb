e140fc94771673bd69eecbac12ab3940df5c68cd
Mark Turner <mark@amerine.net>
2010-10-16 13:53:38 -0700

    Mixed up my rspec and shoulda worlds

--- spec/models/pint_spec.rb
+++ spec/models/pint_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
 describe Pint do
-
-  context "associations" do
-    should belong_to :user
-    should belong_to :business
-    should belong_to :purchaser
-  end
-
+  it { should belong_to(:user) }
+  it { should belong_to(:business) }
+  it { should belong_to(:purchaser) }
 end
