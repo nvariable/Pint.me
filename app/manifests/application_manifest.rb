406575d22eeb89fd5cdb671504aef5c1d5979edb
Mark Turner <mark@amerine.net>
2010-10-16 00:34:05 -0700

    more hacking around to get moonshine to install rails3

--- app/manifests/application_manifest.rb
+++ app/manifests/application_manifest.rb
@@ -24,6 +24,7 @@ class ApplicationManifest < Moonshine::Manifest::Rails
 
   # Add your application's custom requirements here
   def application_packages
+    recipe :mysql_server, :mysql_gem, :mysql_database, :mysql_user, :mysql_fixup_debian_start
     # If you've already told Moonshine about a package required by a gem with
     # :apt_gems in <tt>moonshine.yml</tt> you do not need to include it here.
     # package 'some_native_package', :ensure => :installed
