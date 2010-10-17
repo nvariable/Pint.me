3f79588a79fe59f9e4276d2ffb09a872597dd8a5
Tim Linquist <timo3377@gmail.com>
2010-10-17 16:14:34 -0700

    Added additional tweet to prevent dup status update errors

--- app/models/order.rb
+++ app/models/order.rb
@@ -37,6 +37,7 @@ class Order < ActiveRecord::Base
     oauth.authorize_from_access(PINT_ME.token, PINT_ME.secret)
     client = Twitter::Base.new(oauth)
     plural= (quantity >1) ? 'pints' : 'pint'
-    client.update("@#{recipient.screen_name} was just given #{quantity} #{plural} from @#{purchaser.screen_name}.")
+    client.update("@#{recipient.screen_name} was just given #{quantity} #{plural} from @#{purchaser.screen_name}. Visit http://pintme.com to redeem your pint.")
+    client.update("#{Pint.count} pints have been given so far!")
   end
 end
