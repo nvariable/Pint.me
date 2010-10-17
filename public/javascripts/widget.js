f59c75e716d01dddba3c5de7fbacb4cde4fc93bb
Mark Turner <mark@amerine.net>
2010-10-17 11:11:09 -0700

    Added widget stuffs

---
+++ public/javascripts/widget.js
@@ -0,0 +1,12 @@
+var pintmeWidget = document.createElement('DIV');
+pintmeWidget.setAttribute('id', 'pintmeWidget');
+pintmeWidget.setAttrubute('style', 'background: url(\'http://pint.me/images/widgets/widget1.png\');
+
+if (typeof pintmeUser != 'undefined) {
+  var pintmeLink = document.createElement('A');
+  pintmeLink.setAttribute('href', 'http://pint.me/buy/' + pintmeUser);
+  pintmeLink.appendChild(pintmeWidget);
+  document.body.appendChild(pintmeLink);
+} else {
+  alert("Missing PintMe User");
+}
