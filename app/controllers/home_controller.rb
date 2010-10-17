7cfbd369a1193752eef6add2d230a1f924381323
Mark Turner <mark@amerine.net>
2010-10-17 09:29:10 -0700

    made the user searching better

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,7 +12,7 @@ class HomeController < ApplicationController
     oauth = Twitter::OAuth.new(internal_provider.configs['twitter']['consumer_key'], internal_provider.configs['twitter']['consumer_secret'])
     oauth.authorize_from_access(current_user.token, current_user.secret)
     client = Twitter::Base.new(oauth)
-    @users = client.user_search params[:q]
+    @users = client.user_search params[:q] if params[:q].length > 1
     render :layout => false
   end
 end
