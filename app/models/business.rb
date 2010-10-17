50f47213c754861b9b949be036a8e0ef6a4dbe96
Tim Linquist <timo3377@gmail.com>
2010-10-16 23:43:30 -0700

    Added geocoding functionality & assoc to biz

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,9 @@
+require 'forwardable'
+
 class Business < ActiveRecord::Base
+  extend Forwardable
+  def_delegators :@geocode, :latitude, :longitude
+  
   has_many :pints
 
   validates_numericality_of :secret_code
@@ -9,4 +14,12 @@ class Business < ActiveRecord::Base
   validates_presence_of :city
   validates_presence_of :state
   validates_presence_of :zip
+  
+  def to_s
+    "#{street_1}, #{city}, #{state}"
+  end  
+  
+  def geocode
+    @geocode ||= Geocode.new(self.to_s)
+  end
 end
