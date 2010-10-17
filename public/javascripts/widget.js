0acf9a7c0aa4a179ee3ec386fcb6e0f6a2094e30
Mark Turner <mark@amerine.net>
2010-10-17 11:18:46 -0700

    widget bug

--- public/javascripts/widget.js
+++ public/javascripts/widget.js
@@ -2,7 +2,7 @@ var pintmeWidget = document.createElement('DIV');
 pintmeWidget.setAttribute('id', 'pintmeWidget');
 pintmeWidget.setAttrubute('style', 'background: url(\'http://pint.me/images/widgets/widget1.png\');');
 
-if (typeof pintmeUser != 'undefined) {
+if (typeof pintmeUser != 'undefined') {
   var pintmeLink = document.createElement('A');
   pintmeLink.setAttribute('href', 'http://pint.me/buy/' + pintmeUser);
   pintmeLink.appendChild(pintmeWidget);
